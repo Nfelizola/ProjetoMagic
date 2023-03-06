41bd93c4cd464757f35107fdd97b34c22bd3d785
Nivaldo Felizola <55410946+Nfelizola@users.noreply.github.com>
2023-03-06 16:18:14 -0300

    ajuste para dinastina NEO

--- js/geracarta.js
+++ js/geracarta.js
@@ -6,27 +6,60 @@ https://api.scryfall.com/cards/dom/2/pt
 const urlbase = "https://api.scryfall.com/cards/"
 const urlapirandomica = "https://api.scryfall.com/cards/random";
 
+
 // let searchPokemon = 1;
 // let mtg=386616;
 function getcard(url,n) {
  const sorteio = fetch(`${url}`)
     .then(response => response.json())
     .then(data => {
-      document.getElementById('c'+n).src=data.image_uris.small;
+      gettraduzida(data.set,data.collector_number,n);
+  
+      }
+      
+      )
+  
+}
+function gettraduzida(s,m,n){
+  let urlpt = urlbase+s+'/'+m+'/pt';  
+  alert(urlpt);
+  const sorteio = fetch(`${urlpt}`)
+    .then(response => response.json())
+    .then(data => {
       document.getElementById('o'+n).src=data.image_uris.png;
+      document.getElementById('c'+n).src=data.image_uris.small;
+  
       }
       
       )
   
 }
 
+ function getcartneo(url,i){
+  const sorte = fetch(`${url}`)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('o'+i).src=data.image_uris.png;
+      document.getElementById('c'+i).src=data.image_uris.small;
+  
+      }
+      
+      )
+ } 
+
+
+
 botaoiniciar.addEventListener('click', () => {
-
-  let x = parseInt(Math.random() * 3);
-  alert (x);
-
-  for (i = x+1; i < 8; i++) {
-    getcard(urlapirandomica,i);
+  for (i = 1; i < 8; i++) {
+    var moeda = parseInt(Math.random() * 2)
+    var land = 283+ parseInt(Math.random() * 11)
+    var nx = parseInt(Math.random() * 294)+1;
+    if (moeda > 0) {
+      nx = land;
+  
+   }
+    let urlneo = urlbase+'neo/'+nx+'/pt';
+    getcartneo(urlneo,i);
 
   }
   
