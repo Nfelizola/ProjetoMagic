1d5af97e90fe94a31f98f6a09ab6842ec71700cc
Nivaldo Felizola <55410946+Nfelizola@users.noreply.github.com>
2023-03-08 11:24:17 -0300

    finalizado

--- js/geracarta.js
+++ js/geracarta.js
@@ -67,14 +67,28 @@ function getpexels(){
       )
  } 
 
+ function drawneo(url){
+  const sorte = fetch(`${url}`)
+    .then(response => response.json())
+    .then(data => {
+      var urlcompos = data.image_uris.small;
+      document.getElementById('draw').src=urlcompos;
+      document.getElementById('draw').style.visibility='visible';
+      
+  
+      }
+      
+      )
+ } 
+
 
 
 botaoiniciar.addEventListener('click', () => {
   for (i = 1; i < 8; i++) {
-    var moeda = parseInt(Math.random() * 2)
+    var moeda = parseInt(Math.random() * 1)
     var land = 283+ parseInt(Math.random() * 11)
     var nx = parseInt(Math.random() * 294)+1;
-    if (moeda > 0) {
+    if (moeda =0) {
       nx = land;
   
    }
@@ -87,9 +101,19 @@ botaoiniciar.addEventListener('click', () => {
   })
 
 
-  botaopexel.addEventListener('click', () => {
+botaopexel.addEventListener('click', () => {
     getpexels()})
 
 
 
-
+comprar1.addEventListener('click', () => {
+        var moeda = Math.random() * 1;
+        var land = 283+ parseInt(Math.random() * 11)
+        var nx = parseInt(Math.random() * 294)+1;
+        if (moeda > 0.40) {
+          nx = land;      
+       }
+        let urlneo = urlbase+'neo/'+nx+'/pt';
+        drawneo(urlneo,i);
+    
+      })
